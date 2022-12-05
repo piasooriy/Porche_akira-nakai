14496eb5f35f9dfd9cc481dad35b0064145561eb
Pia sooriy <112381491+piasooriy@users.noreply.github.com>
2022-12-05 19:08:00 +0100

    corrected BEM and wrote some comments

--- js/modules/contact.js
+++ js/modules/contact.js
@@ -4,21 +4,25 @@ export default function Contact () {
 
 	/* querySelector */
 
+	//Choosing the HTML elements I want to target
 	const contactButton = document.querySelector('.navigation__contact-button');
 	const contactSection = document.querySelector('.contact__section');
 
 
 	/* EventListener */
+
+	//Adding an "action" -> a click
 	contactButton.addEventListener('click', handleContactButtonOnClick);
 
+
 	/* EventHandler */
+	//choosing what the "action" should do
 	function handleContactButtonOnClick (event) {
 		toggleContact();
 		renderContactHTML();
 	}
 
 	/* Methods */
-
 	function toggleContact() {
 		contactVisible = !contactVisible;
 	}
@@ -29,6 +33,8 @@ export default function Contact () {
 	//Maybe if I come back to it later it will work?
 	//Never mind, it works now
 
+	//the action is to add  "--visible" from css(classlist)
+	//and to remove it when clicked a second time. 
 	function renderContactHTML() {
 		if (contactVisible === true) {
 			contactSection.classList.add('contact__section--visible');
